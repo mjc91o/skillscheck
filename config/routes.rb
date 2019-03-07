63668aec643dea1cd0c8eeab1da90dc19a3d42cd
Michael Christian <mike@WALDO.local>
2019-03-07 10:10:32 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :comments
+  resources :posts
+  get 'index' => 'practice#index'
+  get 'about' => 'practice#about'
+  post 'index' => 'practice#index'
+
+  root 'posts#index'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
